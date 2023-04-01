dd3640586e6b81b9d387b98c5d5b0a068273a755
Ismo Vuorinen <ismo@ivuorinen.net>
2023-04-01 22:26:03 +0300

    Dropped dotbot-ifplatform and dotbot-golang

---
+++ scripts/install-go-packages.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Install Go packages
+#
+# shellcheck source=shared.sh
+source "$HOME/.dotfiles/scripts/shared.sh"
+
+if ! command -v go &> /dev/null; then
+  msg "go hasn't been installed yet."
+else
+  packages=(
+    # sysadmin/scripting utilities, distributed as a single binary
+    github.com/skx/sysbox@latest
+    github.com/dotzero/git-profile@latest
+    github.com/google/yamlfmt/cmd/yamlfmt@latest
+  )
+
+  for pkg in "${packages[@]}"; do
+    # Trim spaces
+    pkg=${pkg// /}
+    # Skip comments
+    if [[ ${pkg:0:1} == "#" ]]; then continue; fi
+
+    msg_run "Installing go package:" "$pkg"
+    go install "$pkg"
+    echo ""
+  done
+
+  msg_ok "Done"
+
+fi
