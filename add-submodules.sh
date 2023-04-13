dcd042262f8f80368a36d554e8730f90da7c8a27
Ismo Vuorinen <ismo@ivuorinen.net>
2023-04-13 22:21:49 +0300

    dotbot: removed gh-extension, use old script

--- add-submodules.sh
+++ add-submodules.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 git submodule add --name dotbot-brew -f https://github.com/wren/dotbot-brew.git dotbot-brew
-git submodule add --name dotbot-gh-extension -f https://github.com/fundor333/dotbot-gh-extension.git dotbot-gh-extension
 git submodule add --name dotbot-include -f https://gitlab.com/gnfzdz/dotbot-include.git dotbot-include
