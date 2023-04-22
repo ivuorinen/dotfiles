775d2abf134e19960e40e57f70f39d86c00bf659
Ismo Vuorinen <ismo@ivuorinen.net>
2023-04-22 11:17:36 +0300

    tools: cheat configs & dirs

--- add-submodules.sh
+++ add-submodules.sh
@@ -2,3 +2,4 @@
 
 git submodule add --name dotbot-brew -f https://github.com/wren/dotbot-brew.git dotbot-brew
 git submodule add --name dotbot-include -f https://gitlab.com/gnfzdz/dotbot-include.git dotbot-include
+git submodule add --name cheat-community -f https://github.com/cheat/cheatsheets.git config/cheat/cheatsheets/community
