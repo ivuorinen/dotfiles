74df61ed1d716a77a1512a3fad11036d6667eeaa
Ismo Vuorinen <ismo@ivuorinen.net>
2023-04-26 23:45:28 +0300

    Feat: tmux config + plugin manager

--- add-submodules.sh
+++ add-submodules.sh
@@ -3,3 +3,5 @@
 git submodule add --name dotbot-brew -f https://github.com/wren/dotbot-brew.git dotbot-brew
 git submodule add --name dotbot-include -f https://gitlab.com/gnfzdz/dotbot-include.git dotbot-include
 git submodule add --name cheat-community -f https://github.com/cheat/cheatsheets.git config/cheat/cheatsheets/community
+git submodule add --name tmux/tpm -f https://github.com/tmux-plugins/tpm.git config/tmux/plugins/tpm
+
