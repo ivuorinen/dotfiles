263fca58397cab9301c9e2824a8d343fed9590d1
Ismo Vuorinen <ismo@ivuorinen.net>
2023-04-27 01:12:31 +0300

    Feat: Configuration updates

--- config/astronvim/lua/user/plugins/community.lua
+++ config/astronvim/lua/user/plugins/community.lua
@@ -1,7 +1,12 @@
 return { -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity", -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    import = "astrocommunity.colorscheme.catppuccin",
+    opts = {
+      flavour = "mocha"
+    }
+  },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   {
@@ -12,14 +17,16 @@ return { -- Add the community repository of plugin specifications
     },
   },
   { import = "astrocommunity.code-runner.overseer-nvim" },
-  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
-  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  --- { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  --- { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.editing-support.refactoring-nvim" },
   { import = "astrocommunity.editing-support.neogen" },
   { import = "astrocommunity.editing-support.nvim-regexplainer" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+  { import = "astrocommunity.pack.php" },
+  { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.project.neoconf-nvim" },
   { import = "astrocommunity.test.neotest" },
   { import = "astrocommunity.utility.neodim" },
--- config/astronvim/lua/user/plugins/user.lua
+++ config/astronvim/lua/user/plugins/user.lua
@@ -1,17 +1,6 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    lazy = false,
-    enabled = true,
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha"
-      })
-    end,
-  },
   "andweeb/presence.nvim",
   {
     "ray-x/lsp_signature.nvim",
