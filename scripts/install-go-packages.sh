0feb7a47ae1703b25a81893cde391a5277cf0dda
Ismo Vuorinen <ismo@ivuorinen.net>
2023-04-27 15:09:34 +0300

    Chore: Document go packages, add glow

--- scripts/install-go-packages.sh
+++ scripts/install-go-packages.sh
@@ -8,11 +8,18 @@ have go && {
   packages=(
     # sysadmin/scripting utilities, distributed as a single binary
     github.com/skx/sysbox@latest
+    # Git Profile allows you to switch between user profiles in git repos
     github.com/dotzero/git-profile@latest
+    # An extensible command line tool or library to format yaml files.
     github.com/google/yamlfmt/cmd/yamlfmt@latest
+    # Parsing HTML at the command line
     github.com/ericchiang/pup@latest
+    # HTML to Markdown converter
     github.com/suntong/html2md@latest
+    # cheat allows you to create and view interactive cheatsheets on the cli.
     github.com/cheat/cheat/cmd/cheat@latest
+    # Render markdown on the CLI, with pizzazz! 💅
+    github.com/charmbracelet/glow@latest
   )
 
   for pkg in "${packages[@]}"; do
