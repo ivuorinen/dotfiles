df14c71c173e4cc256e7471d5728f18cea613ea7
Ismo Vuorinen <ismo@ivuorinen.net>
2023-04-27 16:38:26 +0300

    Feat: Install neofetch

---
+++ scripts/install-neofetch.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#
+# Install neofetch from source
+#
+# shellcheck source=shared.sh
+source "$HOME/.dotfiles/scripts/shared.sh"
+
+NEOFETCH_VERSION="7.1.0"
+NEOFETCH_REPO="https://github.com/dylanaraps/neofetch"
+NEOFETCH_URL="${NEOFETCH_REPO}/archive/refs/tags/${NEOFETCH_VERSION}.tar.gz"
+NEOFETCH_TEMP="/tmp/neofetch"
+NEOFETCH_INSTALL_PREFIX="$HOME/.local"
+
+have neofetch || {
+  LC_ALL=C
+
+  mkdir -p "$NEOFETCH_TEMP" "$NEOFETCH_INSTALL_PREFIX"
+
+  curl -L "$NEOFETCH_URL" > "$NEOFETCH_TEMP.tar.gz"
+  tar zxvf "$NEOFETCH_TEMP.tar.gz" --directory="$NEOFETCH_TEMP"
+  cd "$NEOFETCH_TEMP/neofetch-$NEOFETCH_VERSION" \
+    && make PREFIX="${NEOFETCH_INSTALL_PREFIX}" install \
+    && rm -rf "$NEOFETCH_TEMP*" \
+    && msg_yay "neofetch installed!"
+}
