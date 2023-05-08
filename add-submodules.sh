b688ee7503872b7a9963a52606c52abd3c3cd233
Ismo Vuorinen <ismo@ivuorinen.net>
2023-05-08 12:16:45 +0300

    Chore(tools): Moved dotbot under tools

--- add-submodules.sh
+++ add-submodules.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-git submodule add --name dotbot-brew -f https://github.com/wren/dotbot-brew.git dotbot-brew
-git submodule add --name dotbot-include -f https://gitlab.com/gnfzdz/dotbot-include.git dotbot-include
+git submodule add --name dotbot -f https://github.com/anishathalye/dotbot.git tools/dotbot
+git submodule add --name dotbot-brew -f https://github.com/wren/dotbot-brew.git tools/dotbot-brew
+git submodule add --name dotbot-include -f https://gitlab.com/gnfzdz/dotbot-include.git tools/dotbot-include
 git submodule add --name cheat-community -f https://github.com/cheat/cheatsheets.git config/cheat/cheatsheets/community
 git submodule add --name tmux/tpm -f https://github.com/tmux-plugins/tpm.git config/tmux/plugins/tpm
 
