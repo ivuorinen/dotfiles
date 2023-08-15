7493fd640147f504354edce3bdddadb58f2ce1d0
Ismo Vuorinen <ismo@ivuorinen.net>
2023-08-15 12:17:30 +0300

    fix(config): fix fzf.zsh paths

--- config/fzf/fzf.zsh
+++ config/fzf/fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/ivuorinen/.config/fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/ivuorinen/.config/fzf/bin"
+if [[ ! "$PATH" == *$HOME/.config/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}$HOME/.config/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/ivuorinen/.config/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.config/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/ivuorinen/.config/fzf/shell/key-bindings.zsh"
+source "$HOME/.config/fzf/shell/key-bindings.zsh"
