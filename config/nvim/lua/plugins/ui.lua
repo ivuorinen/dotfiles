cf92bc9a692b3d82fe63c07f58f2cdcde71d1c11
Ismo Vuorinen <ismo@ivuorinen.net>
2023-09-18 09:23:49 +0300

    feat(nvim): anuvyklack/windows.nvim

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -1,3 +1,4 @@
+-- luacheck: globals vim
 return {
   -- The theme of choise, catppuccin
   -- https://github.com/catppuccin/nvim
@@ -22,4 +23,20 @@ return {
   -- Neovim plugin for locking a buffer to a window
   -- https://github.com/stevearc/stickybuf.nvim
   { "stevearc/stickybuf.nvim", opts = {} },
+  -- Automatically expand width of the current window.
+  -- Maximizes and restore it. And all this with nice animations!
+  -- https://github.com/anuvyklack/windows.nvim
+  {
+    "anuvyklack/windows.nvim",
+    dependencies = {
+      "anuvyklack/middleclass",
+      "anuvyklack/animation.nvim",
+    },
+    config = function()
+      vim.o.winwidth = 10
+      vim.o.winminwidth = 10
+      vim.o.equalalways = false
+      require("windows").setup()
+    end,
+  },
 }
