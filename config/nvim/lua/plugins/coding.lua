00de4a7b83245d1127ba63d9ba2be7789c796e4a
Ismo Vuorinen <ismo@ivuorinen.net>
2023-09-19 01:34:42 +0300

    chore(nvim): plugins, updates

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -75,6 +75,7 @@ return {
   {
     "codota/tabnine-nvim",
     name = "tabnine",
+    enabled = false,
     build = vim.loop.os_uname().sysname == "Windows_NT" and "pwsh.exe -file .\\dl_binaries.ps1" or "./dl_binaries.sh",
     cmd = { "TabnineStatus", "TabnineDisable", "TabnineEnable", "TabnineToggle" },
     event = "User",
