0aff4784b14a74e5c1b7118806ea25bd272e9fc1
Ismo Vuorinen <ismo@ivuorinen.net>
2023-09-28 17:13:44 +0300

    feat(scripts): msgr helper script

--- scripts/install-pip-packages.sh
+++ scripts/install-pip-packages.sh
@@ -4,6 +4,8 @@
 # shellcheck source=shared.sh
 source "$HOME/.dotfiles/scripts/shared.sh"
 
+msg "Starting to install pip packages"
+
 ! have pip && {
   msg_err "Could not find pip, something really terrible is going on." && exit 1
 }
@@ -19,7 +21,10 @@ for pkg in "${packages[@]}"; do
   # Skip comments
   if [[ ${pkg:0:1} == "#" ]]; then continue; fi
 
+  msg_nested "Installing pip package: $pkg"
   python3 -m pip install --user "$pkg"
 
   echo ""
 done
+
+msg_yay "Run pip package installation"
