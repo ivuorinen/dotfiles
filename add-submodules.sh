9c432ed2a06bdfe16b00e4dd0c3863f651b5cef9
Ismo Vuorinen <ismo@ivuorinen.net>
2023-10-02 11:32:41 +0300

    feat(tmux): update cfg, remove tpm, change theme

--- add-submodules.sh
+++ add-submodules.sh
@@ -19,14 +19,8 @@ git submodule add --name cheat-community \
   -f https://github.com/cheat/cheatsheets.git config/cheat/cheatsheets/community
 
 # tmux plugin manager and plugins
-git submodule add --name tmux/tpm \
-  -f https://github.com/tmux-plugins/tpm.git config/tmux/plugins/tpm
-git submodule add --name tmux/catppuccin \
-  -f https://github.com/catppuccin/tmux.git config/tmux/plugins/tmux
 git submodule add --name tmux/tmux-continuum \
   -f https://github.com/tmux-plugins/tmux-continuum config/tmux/plugins/tmux-continuum
-git submodule add --name tmux/tmux-menus \
-  -f https://github.com/jaclu/tmux-menus.git config/tmux/plugins/tmux-menus
 git submodule add --name tmux/tmux-resurrect \
   -f https://github.com/tmux-plugins/tmux-resurrect config/tmux/plugins/tmux-resurrect
 git submodule add --name tmux/tmux-sensible \
@@ -37,3 +31,8 @@ git submodule add --name tmux/tmux-window-name \
   -f https://github.com/ofirgall/tmux-window-name.git config/tmux/plugins/tmux-window-name
 git submodule add --name tmux/tmux-yank \
   -f https://github.com/tmux-plugins/tmux-yank.git config/tmux/plugins/tmux-yank
+
+# remove old submodules
+[ -d "config/tmux/plugins/tpm" ] && rm -rf config/tmux/plugins/tpm
+[ -d "config/tmux/plugins/tmux" ] && rm -rf config/tmux/plugins/tmux
+[ -d "config/tmux/plugins/tmux-menus" ] && rm -rf config/tmux/plugins/tmux-menus
