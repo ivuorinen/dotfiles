cef15eddce2dad561e8979efb9b200b71112ac74
Ismo Vuorinen <ismo@ivuorinen.net>
2024-07-09 03:20:50 +0300

    chore(asdf): update tools, fix installer

--- scripts/install-asdf.sh
+++ scripts/install-asdf.sh
@@ -34,42 +34,42 @@ asdf-plugin-manager add-all
 
 asdf install
 
-# ASDF_INSTALLABLES=(
-#     "nodejs:github.com/asdf-vm/asdf-nodejs.git"
-#     "1password-cli:github.com/NeoHsu/asdf-1password-cli.git"
-#     "age:github.com/threkk/asdf-age.git"
-#     "bottom:github.com/carbonteq/asdf-btm.git"
-#     "dotenv-linter:github.com/wesleimp/asdf-dotenv-linter.git"
-#     "editorconfig-checker:github.com/gabitchov/asdf-editorconfig-checker.git"
-#     "eza:github.com/lwiechec/asdf-eza.git"
-#     "fd:gitlab.com/wt0f/asdf-fd.git"
-#     "github-cli:github.com/bartlomiejdanek/asdf-github-cli.git"
-#     "hadolint:github.com/devlincashman/asdf-hadolint.git"
-#     "kubectl:github.com/asdf-community/asdf-kubectl.git"
-#     "lazygit:github.com/nklmilojevic/asdf-lazygit.git"
-#     "pipx:github.com/yozachar/asdf-pipx.git"
-#     "pre-commit:github.com/jonathanmorley/asdf-pre-commit.git"
-#     "ripgrep:gitlab.com/wt0f/asdf-ripgrep.git"
-#     "semgrep:github.com/brentjanderson/asdf-semgrep.git"
-#     "terraform-ls:github.com/asdf-community/asdf-hashicorp.git"
-#     "vault:github.com/asdf-community/asdf-hashicorp.git"
-#     "shellcheck:github.com/luizm/asdf-shellcheck.git"
-#     "shfmt:github.com/luizm/asdf-shfmt.git"
-#     "terraform-lsp:github.com/bartlomiejdanek/asdf-terraform-lsp.git"
-#     "terragrunt:github.com/ohmer/asdf-terragrunt.git"
-#     "tf-summarize:github.com/adamcrews/asdf-tf-summarize.git"
-#     "yamllint:github.com/ericcornelissen/asdf-yamllint.git"
-#     "yq:github.com/sudermanjr/asdf-yq.git"
-# )
-#
-# msg "Installing asdf plugins, if not already installed"
-# for item in "${ASDF_INSTALLABLES[@]}"; do
-#   CMD=$(echo "${item}" | awk -F ":" '{print $1}')
-#   URL=$(echo "${item}" | awk -F ":" '{print $2}')
-#   asdf plugin add "${CMD}" "https://${URL}"
-#   asdf install "${CMD}" latest
-#   asdf global "${CMD}" "$(asdf latest "${CMD}")"
-# done
+ASDF_INSTALLABLES=(
+  "nodejs:github.com/asdf-vm/asdf-nodejs.git"
+  "1password-cli:github.com/NeoHsu/asdf-1password-cli.git"
+  "age:github.com/threkk/asdf-age.git"
+  "bottom:github.com/carbonteq/asdf-btm.git"
+  "dotenv-linter:github.com/wesleimp/asdf-dotenv-linter.git"
+  "editorconfig-checker:github.com/gabitchov/asdf-editorconfig-checker.git"
+  "eza:github.com/lwiechec/asdf-eza.git"
+  "fd:gitlab.com/wt0f/asdf-fd.git"
+  "github-cli:github.com/bartlomiejdanek/asdf-github-cli.git"
+  "hadolint:github.com/devlincashman/asdf-hadolint.git"
+  "kubectl:github.com/asdf-community/asdf-kubectl.git"
+  "lazygit:github.com/nklmilojevic/asdf-lazygit.git"
+  "pipx:github.com/yozachar/asdf-pipx.git"
+  "pre-commit:github.com/jonathanmorley/asdf-pre-commit.git"
+  "ripgrep:gitlab.com/wt0f/asdf-ripgrep.git"
+  "semgrep:github.com/brentjanderson/asdf-semgrep.git"
+  "terraform-ls:github.com/asdf-community/asdf-hashicorp.git"
+  "vault:github.com/asdf-community/asdf-hashicorp.git"
+  "shellcheck:github.com/luizm/asdf-shellcheck.git"
+  "shfmt:github.com/luizm/asdf-shfmt.git"
+  "terraform-lsp:github.com/bartlomiejdanek/asdf-terraform-lsp.git"
+  "terragrunt:github.com/ohmer/asdf-terragrunt.git"
+  "tf-summarize:github.com/adamcrews/asdf-tf-summarize.git"
+  "yamllint:github.com/ericcornelissen/asdf-yamllint.git"
+  "yq:github.com/sudermanjr/asdf-yq.git"
+)
+
+msg "Installing asdf plugins, if not already installed"
+for item in "${ASDF_INSTALLABLES[@]}"; do
+  CMD=$(echo "${item}" | awk -F ":" '{print $1}')
+  URL=$(echo "${item}" | awk -F ":" '{print $2}')
+  asdf plugin add "${CMD}" "https://${URL}"
+  asdf install "${CMD}" latest
+  asdf global "${CMD}" "$(asdf latest "${CMD}")"
+done
 
 msg "Reshim asdf"
 asdf reshim
