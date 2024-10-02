ea499928f25a887347bace852bece51128184b70
Ismo Vuorinen <ismo@ivuorinen.net>
2024-10-03 01:32:19 +0300

    feat(nvim): another configuration tweak round
    added snippets!

---
+++ config/nvim/lua/plugins/neogen.lua
@@ -0,0 +1,8 @@
+-- A better annotation generator.
+-- Supports multiple languages and annotation conventions.
+-- https://github.com/danymat/neogen
+return {
+  'danymat/neogen',
+  version = '*',
+  opts = { enabled = true, snippet_engine = 'luasnip' },
+}
