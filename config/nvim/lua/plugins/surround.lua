0bf1c1ce2455334960bc58dd54387be0650146ca
Ismo Vuorinen <ismo@ivuorinen.net>
2024-12-20 19:13:10 +0200

    fix: messed up rebase somewhere, removing files

--- config/nvim/lua/plugins/surround.lua
+++
@@ -1,8 +0,0 @@
--- Add/change/delete surrounding delimiter pairs with ease.
--- https://github.com/kylechui/nvim-surround
-return {
-  'kylechui/nvim-surround',
-  version = '*',
-  event = 'VeryLazy',
-  opts = {},
-}
