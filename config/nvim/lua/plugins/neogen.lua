42e5f06ac6b4f91a899011396436fd7b3217bae3
Ismo Vuorinen <ismo@ivuorinen.net>
2024-12-20 19:13:10 +0200

    fix: messed up rebase somewhere, removing files

--- config/nvim/lua/plugins/neogen.lua
+++
@@ -1,8 +0,0 @@
--- A better annotation generator.
--- Supports multiple languages and annotation conventions.
--- https://github.com/danymat/neogen
-return {
-  'danymat/neogen',
-  version = '*',
-  opts = { enabled = true, snippet_engine = 'luasnip' },
-}
--- config/nvim/lua/plugins/treesj.lua
+++
@@ -1,9 +0,0 @@
--- Neovim plugin for splitting/joining blocks of code
--- https://github.com/Wansmer/treesj
-return {
-  'Wansmer/treesj',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  opts = {
-    use_default_keymaps = false,
-  },
-}
