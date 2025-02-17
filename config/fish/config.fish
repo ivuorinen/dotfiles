894ec12fd868d7b45df34ceec91ecc0115a5e684
Ismo Vuorinen <ismo@ivuorinen.net>
2025-02-17 12:17:45 +0200

    chore: move asdf to ./local, update configs
    Signed-off-by: Ismo Vuorinen <ismo@ivuorinen.net>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,15 @@
+# ╭──────────────────────────────────────────────────────────╮
+# │                     fish/config.fish                     │
+# ╰──────────────────────────────────────────────────────────╯
+
+# ASDF configuration code
+source $HOME/.local/asdf/asdf.fish
+
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/.local/share/nvim/mason/bin
+fish_add_path $HOME/.local/state/composer/vendor/bin
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-  fish_add_path $HOME/.cargo/bin
-  fish_add_path $HOME/.local/bin
-  fish_add_path $HOME/.local/share/nvim/mason/bin
-  fish_add_path $HOME/.local/state/composer/vendor/bin
+  # Commands to run in interactive sessions can go here
 end
