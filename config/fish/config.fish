1b6a93650b53bd4ae84e8c6f7b9764917d827883
Ismo Vuorinen <ismo@ivuorinen.net>
2025-02-19 10:15:01 +0200

    chore(config): fish config tweaks and function fix
    Signed-off-by: Ismo Vuorinen <ismo@ivuorinen.net>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,7 +5,6 @@
 # ASDF configuration code
 source $HOME/.local/asdf/asdf.fish
 
-fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.local/share/nvim/mason/bin
 fish_add_path $HOME/.local/state/composer/vendor/bin
