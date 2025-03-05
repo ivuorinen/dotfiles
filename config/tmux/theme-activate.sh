26e11047ced2dc8673fd380ab43e2d13f1383f98
Ismo Vuorinen <ismo@ivuorinen.net>
2025-03-05 15:07:20 +0200

    chore(config): tweak tmux config & theme activator

--- config/tmux/theme-activate.sh
+++ config/tmux/theme-activate.sh
@@ -12,5 +12,9 @@
 # License: MIT
 
 THEME_FILE="$HOME/.local/state/tmux/tmux-dark-notify-theme.conf"
+THEME_BACKUP="$HOME/.dotfiles/config/tmux/theme-dark.conf"
+
+# If THEME_FILE doesn't exist, link $THEME_BACKUP to it
+[[ ! -e $THEME_FILE ]] && ln -s "$THEME_BACKUP" "$THEME_FILE"
 
 [[ -e $THEME_FILE ]] && tmux source-file "$THEME_FILE"
