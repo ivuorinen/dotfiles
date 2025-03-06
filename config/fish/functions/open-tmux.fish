9f71f3c8aa388751302b62a0af5315a20e9204ac
Ismo Vuorinen <ismo@ivuorinen.net>
2025-03-06 14:07:29 +0200

    chore(config): simplify fish open-tmux function
    Signed-off-by: Ismo Vuorinen <ismo@ivuorinen.net>

--- config/fish/functions/open-tmux.fish
+++ config/fish/functions/open-tmux.fish
@@ -2,9 +2,8 @@
 # Dependencies: tmux
 # Usage: open-tmux
 function open-tmux --wraps='tmux attach-session -t main || tmux new-session -s main' --description 'open tmux session'
-  if test -z "$SSH_TTY"
-    if not set -q TMUX
-      tmux attach-session -t main || tmux new-session -s main
-    end
+  # Check if not in an SSH session and not already in a tmux session
+  if test -z "$SSH_TTY"; and not set -q TMUX
+    command tmux attach-session -t main || command tmux new-session -s main
   end
 end
