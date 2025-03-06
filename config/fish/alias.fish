afc04db93a6f1a62c9190d128a75393d38c51c70
Ismo Vuorinen <ismo@ivuorinen.net>
2025-03-06 14:04:43 +0200

    chore(config): simplify eza aliases
    Signed-off-by: Ismo Vuorinen <ismo@ivuorinen.net>

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -2,3 +2,30 @@
 
 alias vim='vim -u "$XDG_CONFIG_HOME/vim/vimrc"'
 
+# eza aliases if eza is installed
+if type -q eza > /dev/null
+
+  function eza_git -d "Use eza and its git options if in a git repo"
+      if git rev-parse --is-inside-work-tree &>/dev/null
+          eza --group-directories-first --icons=always --smart-group --git $argv
+      else
+          eza --group-directories-first --icons=always --smart-group $argv
+      end
+  end
+
+  function lsa --wraps='eza_git -al' --description 'eza -al'
+    eza_git -al $argv
+  end
+
+  function ls --wraps='eza_git' --description 'eza'
+    eza_git $argv
+  end
+
+  function ll --wraps='eza_git -l' --description 'eza -l'
+    eza_git -l $argv
+  end
+
+  function l --wraps='eza_git' --description 'eza'
+    eza_git $argv
+  end
+end
--- config/fish/functions/l.fish
+++
@@ -1,4 +0,0 @@
-function l --wraps='exa $EXA_STANDARD_OPTIONS $EXA_L_OPTIONS' --wraps='eza $EZA_STANDARD_OPTIONS $EZA_L_OPTIONS' --description 'alias l eza $EZA_STANDARD_OPTIONS $EZA_L_OPTIONS'
-  eza $EZA_STANDARD_OPTIONS $EZA_L_OPTIONS $argv
-        
-end
