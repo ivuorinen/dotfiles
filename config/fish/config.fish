64725c57dc915d6c4931e73312270012d7da79ab
Ismo Vuorinen <ismo@ivuorinen.net>
2025-04-11 16:31:28 +0300

    chore(config): fish config tweaks

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,18 +9,21 @@ test -e "$HOME/.config/fish/exports.fish" &&
     source "$HOME/.config/fish/exports.fish"
 
 if status is-interactive
-  # Commands to run in interactive sessions can go here
+    # Commands to run in interactive shell
 
-  # Start tmux if not already running and not in SSH
-  open-tmux # defined in functions/open-tmux.fish
+    # version manager initializers
+    type -q rbenv; and source (rbenv init -|psub)
+    type -q pyenv; and source (pyenv init -|psub)
+    type -q pyenv; and source (pyenv virtualenv-init -)
+    type -q goenv; and source (goenv init -|psub)
+    # type -q fnm; and fnm env --use-on-cd --shell fish | source
+    type -q load_nvm; and load_nvm > /dev/stderr
+
+    # Start tmux if not already running and not in SSH
+    open-tmux # defined in functions/open-tmux.fish
 end
 
 # Added by LM Studio CLI (lms)
 set -gx PATH $PATH $HOME/.lmstudio/bin
 
-type -q rbenv; and source (rbenv init -|psub)
-type -q pyenv; and source (pyenv init -|psub)
-type -q goenv; and source (goenv init -|psub)
-type -q fnm; and fnm env --use-on-cd --shell fish | source
-type -q load_nvm; and load_nvm > /dev/stderr
-
+# vim: ft=fish ts=4 sw=4 et:
