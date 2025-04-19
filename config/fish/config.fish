5321ad7bd7a3e659e3d1b8e3d27e1be48d8d51a2
Ismo Vuorinen <ismo@ivuorinen.net>
2025-04-19 04:11:14 +0300

    feat(config): fish formatting, secrets, op
    Signed-off-by: Ismo Vuorinen <ismo@ivuorinen.net>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -8,13 +8,23 @@ test -e "$HOME/.config/fish/alias.fish" &&
 test -e "$HOME/.config/fish/exports.fish" &&
     source "$HOME/.config/fish/exports.fish"
 
+test -e "$HOME/.config/fish/exports.secret.fish" &&
+    source "$HOME/.config/fish/exports.secret.fish"
+
+test -e "$HOME/.dotfiles/config/fzf/key-bindings.fish" &&
+    source "$HOME/.dotfiles/config/fzf/key-bindings.fish"
+
 if status is-interactive
     # Commands to run in interactive shell
+
+    # 1Password plugins if op command is available
+    type -q op; and test -e "$HOME/.config/op/plugins.sh" &&
+        source "$HOME/.config/op/plugins.sh"
 
     # version manager initializers
     type -q rbenv; and source (rbenv init -|psub)
     type -q pyenv; and source (pyenv init -|psub)
-    type -q pyenv; and source (pyenv virtualenv-init -)
+    type -q pyenv; and source (pyenv virtualenv-init -|psub)
     type -q goenv; and source (goenv init -|psub)
     # type -q fnm; and fnm env --use-on-cd --shell fish | source
     type -q load_nvm; and load_nvm > /dev/stderr
