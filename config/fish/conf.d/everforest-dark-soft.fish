91be3b9edc1526645129ff18a4803b92196dcbb6
Ismo Vuorinen <ismo@ivuorinen.net>
2025-10-13 14:16:49 +0300

    feat: everforest themes

---
+++ config/fish/conf.d/everforest-dark-soft.fish
@@ -0,0 +1,24 @@
+# Everforest color scheme for fish shell
+# Generated from template - do not edit manually
+
+# Set fish colors
+set -U fish_color_normal #d3c6aa
+set -U fish_color_command #7fbbb3
+set -U fish_color_keyword #d699b6
+set -U fish_color_quote #dbbc7f
+set -U fish_color_redirection #83c092
+set -U fish_color_end #e69875
+set -U fish_color_error #e67e80
+set -U fish_color_param #d3c6aa
+set -U fish_color_comment #7a8478
+set -U fish_color_selection --background=#3a464c
+set -U fish_color_search_match --background=#3a464c
+set -U fish_color_operator #a7c080
+set -U fish_color_escape #d699b6
+set -U fish_color_autosuggestion #859289
+
+# Set fish pager colors
+set -U fish_pager_color_progress #9da9a0
+set -U fish_pager_color_prefix #7fbbb3
+set -U fish_pager_color_completion #d3c6aa
+set -U fish_pager_color_description #859289
---
+++ config/fish/conf.d/everforest-light-medium.fish
@@ -0,0 +1,24 @@
+# Everforest color scheme for fish shell
+# Generated from template - do not edit manually
+
+# Set fish colors
+set -U fish_color_normal #5c6a72
+set -U fish_color_command #7fbbb3
+set -U fish_color_keyword #d699b6
+set -U fish_color_quote #dbbc7f
+set -U fish_color_redirection #83c092
+set -U fish_color_end #e69875
+set -U fish_color_error #e67e80
+set -U fish_color_param #5c6a72
+set -U fish_color_comment #a6b0a0
+set -U fish_color_selection --background=#ede6cf
+set -U fish_color_search_match --background=#ede6cf
+set -U fish_color_operator #a7c080
+set -U fish_color_escape #d699b6
+set -U fish_color_autosuggestion #b3c0b0
+
+# Set fish pager colors
+set -U fish_pager_color_progress #c0cdb8
+set -U fish_pager_color_prefix #7fbbb3
+set -U fish_pager_color_completion #5c6a72
+set -U fish_pager_color_description #b3c0b0
