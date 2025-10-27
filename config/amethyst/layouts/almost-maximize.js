47a08446ff43b63c748900afd69bd957abaf75a8
Ismo Vuorinen <ismo@ivuorinen.net>
2025-10-27 16:02:40 +0200

    feat: switch to biome, apply formatting, shellcheck
    Signed-off-by: Ismo Vuorinen <ismo@ivuorinen.net>

--- config/amethyst/layouts/almost-maximize.js
+++ config/amethyst/layouts/almost-maximize.js
@@ -10,17 +10,17 @@
  * @param {Object} extendedFrames - The frames of the windows in the current space.
  * @return {Object} - The frames for the windows in the current space.
  */
-function layout() {
+function _layout() {
   return {
     name: 'Almost Maximize',
-    getFrameAssignments: (windows, screenFrame, state, extendedFrames) => {
+    getFrameAssignments: (windows, screenFrame, _state, _extendedFrames) => {
       const width = screenFrame.width * 0.95
       const height = screenFrame.height * 0.95
       const x = (screenFrame.width - width) / 2
       const y = (screenFrame.height - height) / 2
       const windowFrames = {}
 
-      windows.forEach(window => {
+      windows.forEach((window) => {
         windowFrames[window.id] = {
           Y: screenFrame.y + y,
           x: screenFrame.x + x,
